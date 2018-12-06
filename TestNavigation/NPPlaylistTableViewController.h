faa5c40929b9aae9d0a3cf0511e366543594d80b
Nodir <sinigami83@mail.ru>
2018-12-06 16:42:38 +0500

    added MVCs

---
+++ TestNavigation/NPPlaylistTableViewController.h
@@ -0,0 +1,11 @@
+//
+//  Created by Nodir Latipov on 12/6/18.
+//  Copyright © 2018 Latipov Nodir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface NPPlaylistTableViewController : UITableViewController
+
+@end
